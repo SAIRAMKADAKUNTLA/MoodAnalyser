42ee97608f29114f10c1f5c5ec6bd24eca6f905a
SAIRAMKADAKUNTLA <sairam.kadakuntla30@gmail.com>
2021-06-21 19:32:42 +0530

    [Sairam]Added:using reflection we are testing the program temp obj and analyseer

---
+++ Mood.analyser/Mood.analyser/MoodAnalyserFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace MoodAnalyser
+{
+    public class MoodAnalyserFactory
+    {
+        public static object CreateObjectAtRuntime()
+        {
+            try
+            {
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                Type moodAnalyserType = assembly.GetType("MoodAnalyser.MoodAnalyserr");
+                Console.WriteLine($"Name of the class at Runtime: {moodAnalyserType.Name}");
+                object moodAnalyserObject = Activator.CreateInstance(moodAnalyserType);
+                return moodAnalyserObject;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Done Reflections");
+            }
+            return null;
+        }
+    }
+}
--- Mood.analyser/MoodTest/UnitTest1.cs
+++ Mood.analyser/MoodTest/UnitTest1.cs
@@ -1,60 +1,76 @@
 using NUnit.Framework;
 using MoodAnalyser;
+using System.Reflection;
+using System;
 
 namespace MoodTest
 {
     public class Tests
     {
+     
+
+        [Test]
+        public void When_SadMood_ReturnSadMood()
+        {
+            ///Arrange
+            string message = "Im in a Sad mood";
+            string excepted = "SAD";
+            ///Act
+            MoodAnalyserr mood = new MoodAnalyserr(message);
+            string result = mood.MoodCheck();
+            ///Assert
+            Assert.AreEqual(excepted, result);  
+        }
+        [Test]
+        public void When_HappyMood_ReturnHAPPYMood()
+        {
+            ///Arrange
+            string message = "Im in a Happy mood";
+            string excepted = "HAPPY";
+            ///Act
+            MoodAnalyserr mood = new MoodAnalyserr(message);
+            string result = mood.MoodCheck();
+            ///Assert
+            Assert.AreEqual(excepted, result);
+
+        }
+        [Test]
+        public void When_Invalid_ReturnHAPPYMood()
+        {
+            ///Arrange
+            string message = "";
+            string excepted = "HAPPY";
+            ///Act
+            MoodAnalyserr mood = new MoodAnalyserr(message);
+            string result = mood.MoodCheck();
+            ///Assert
+            Assert.AreEqual(excepted, result);
+        }
         [Test]
         public void WhenGivenClassName_ReturnsObject()
         {
-            //Arrange
-            string expected = "MoodAnalyser.MoodAnalyserr";
-            //Act
-            object result = MoodAnalysisFactory.ObjectAtRuntime();
-            //Assert
-            Assert.AreEqual(expected, result.ToString());
-        }
+            try
+            {
+                Assembly assembly = Assembly.GetExecutingAssembly();
+                Type moodAnalyserType = assembly.GetType("MoodAnalyser.MoodAnalyserr");
+                Console.WriteLine(moodAnalyserType.Name);
+                object tempObject = Activator.CreateInstance(moodAnalyserType);
+                //Arrange
+                string expected = (string)tempObject;
+                //Act
+                string result = "MoodAnalyser.MoodAnalyserr";
+                //Assert
+                Assert.AreEqual(expected, result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Done all test cases");
+            }
 
-
-
-
-            //[Test]
-            //public void When_SadMood_ReturnSadMood()
-            //{
-            //    ///Arrange
-            //    string message = "Im in a Sad mood";
-            //    string excepted = "SAD";
-            //    ///Act
-            //    MoodAnalyserr mood = new MoodAnalyserr(message);
-            //    string result = mood.MoodCheck();
-            //    ///Assert
-            //    Assert.AreEqual(excepted, result);  
-            //}
-            //[Test]
-            //public void When_HappyMood_ReturnHAPPYMood()
-            //{
-            //    ///Arrange
-            //    string message = "Im in a Happy mood";
-            //    string excepted = "HAPPY";
-            //    ///Act
-            //    MoodAnalyserr mood = new MoodAnalyserr(message);
-            //    string result = mood.MoodCheck();
-            //    ///Assert
-            //    Assert.AreEqual(excepted, result);
-
-            //}
-            //[Test]
-            //public void When_Invalid_ReturnHAPPYMood()
-            //{
-            //    ///Arrange
-            //    string message = "";
-            //    string excepted = "HAPPY";
-            //    ///Act
-            //    MoodAnalyserr mood = new MoodAnalyserr(message);
-            //    string result = mood.MoodCheck();
-            //    ///Assert
-            //    Assert.AreEqual(excepted, result);
-            // }
         }
     }
+}
